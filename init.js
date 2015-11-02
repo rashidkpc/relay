8d0b06bb0e4cc94d044ce1f91c8ecdea923e76ea
Rashid Khan <github.fliplap@spamgourmet.com>
2015-11-02 16:54:59 -0700

    Wait for elasticsearch, use server client

--- init.js
+++ init.js
@@ -1,9 +1,5 @@
 var Promise = require('bluebird');
 var _ = require('lodash');
-var elasticsearch = require('elasticsearch');
-var client = new elasticsearch.Client({
-  host: 'http://localhost:9200/'
-});
 var config = require('./relay');
 var loadMethods = require('./lib/load_methods.js');
 
@@ -12,23 +8,31 @@ var loadMethods = require('./lib/load_methods.js');
 module.exports = function (server) {
   // Routes and basically anything the server does at start
 
-  client.indices.create({
-    index: config.index,
-    body: {
-      mappings: {
-        event: {
-          _timestamp: { enabled: true }
+  function createRelayIndex () {
+    console.log('Trying to create relay index');
+    server.plugins.elasticsearch.client.indices.create({
+      index: config.index,
+      body: {
+        mappings: {
+          event: {
+            _timestamp: { enabled: true }
+          }
         }
       }
-    }
-  }).catch(function (e) {
-    if (e.body.error.type !== 'index_already_exists_exception') throw e;
-  }).then(function () {
-    var sources = loadMethods('sources');
-    _.each(sources, function (source) {
-      source.start(server);
+    }).catch(function (e) {
+      if (e.body && e.body.error.type === 'index_already_exists_exception') return;
+      console.log('Failed to create relay index. Trying again in 5s');
+      setTimeout(createRelayIndex, 5000);
+    }).then(function (resp) {
+      if (!resp) return;
+      console.log('Index created, starting sources');
+      var sources = loadMethods('sources');
+      _.each(sources, function (source) {
+        source.start(server);
+      });
     });
-  });
+  }
+  createRelayIndex();
 
   server.route({
     method: 'GET',
