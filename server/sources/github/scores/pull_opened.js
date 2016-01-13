a112be23652254e3f512c0b1ee057d233218417b
Chris Cowan <chris@chriscowan.us>
2016-01-13 15:53:19 -0700

    Adding points for opening a PR

---
+++ server/sources/github/scores/pull_opened.js
@@ -0,0 +1,22 @@
+const Score = require('../../../lib/score');
+const _ = require('lodash');
+
+module.exports = new Score ('pull_merged', {
+  fn: event => {
+
+    const isAMatch = event.type === 'PullRequestEvent' &&
+      _.get(event, 'payload.pull_request.state') === 'open';
+
+    if (isAMatch) {
+      return 0.5; // Bonus for opening a pull
+    }
+  },
+  color: '#9E16F2',
+  toHTML: event => {
+    return '<a target="_blank" href="' + event._source.payload.issue.html_url + '">' +
+    event._source.repo.name + '#' +
+    event._source.payload.issue.number + '</a>';
+  }
+});
+
+
