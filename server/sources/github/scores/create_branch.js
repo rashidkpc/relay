41cab37644f4927106f1fdd4f2e49b51a9da9684
Chris Cowan <chris@chriscowan.us>
2016-01-19 15:42:58 -0700

    Adding create brach event

---
+++ server/sources/github/scores/create_branch.js
@@ -0,0 +1,19 @@
+const Score = require('../../../lib/score');
+const _ = require('lodash');
+const scorePerBranch = 0.25;
+
+module.exports = new Score ('create_branch', {
+  fn: (event, actor) => {
+    const type = _.get(event, 'payload.ref_type');
+    if (type === 'branch') {
+      return scorePerBranch;
+    }
+  },
+  color: '#FF4900',
+  toHTML: (event, actor) => {
+    const repoName = _.get(event._source, 'repo.name');
+    const branch = _.get(event._source, 'payload.ref');
+    return `Created branch <a href="https://github.com/${repoName}/tree/${branch}">${repoName}#${branch}</a>`;
+  }
+});
+
