056d980745852b2d51aad604994985d598ba2117
Chris Cowan <chris@chriscowan.us>
2016-01-19 15:14:28 -0700

    Fixing the push event message

--- server/sources/github/scores/push_event.js
+++ server/sources/github/scores/push_event.js
@@ -1,5 +1,6 @@
 const Score = require('../../../lib/score');
 const _ = require('lodash');
+const scorePerCommit = 0.25;
 
 module.exports = new Score ('push_event', {
   fn: (event, actor) => {
@@ -7,14 +8,15 @@ module.exports = new Score ('push_event', {
     if (_.get(event, 'type') === 'PushEvent') {
       let score = 0;
       _.each(event.payload.commits, commit => {
-        if (commit.author.email === actorEmail) score += 0.25;
+        if (commit.author.email === actorEmail) score += scorePerCommit;
       });
       return score;
     }
   },
   color: '#80488C',
   toHTML: (event, actor) => {
-    return '<a target="_blank" href="' + event._source.repo.url + '">' +
-    event._source.repo.name + '</a>';
+    const sha = _.get(event._source, 'payload.head');
+    const repoName = _.get(event._source, 'repo.name');
+    return `Pushed ${event._source.relay_total_score / scorePerCommit} commits to <a href="https://github.com/${repoName}/commit/${sha}">${repoName}</a>`;
   }
 });
