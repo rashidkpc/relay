b8d0a40b435d35231341885886ba5144ed247545
Rashid Khan <github.fliplap@spamgourmet.com>
2016-01-22 11:23:00 -0700

    Correctly show discource messages

--- server/sources/discourse/scores/message.js
+++ server/sources/discourse/scores/message.js
@@ -7,6 +7,7 @@ module.exports = new Score ('post', {
   },
   color: '#1D9A91',
   toHTML: function (event) {
-    return event._source.message;
+    return `<a href="http://discuss.elastic.co/t/${event._source.topic_id}/${event._source.post_number}">
+            discuss#${event._source.post_id}</a>: ${event._source.title}"`;
   }
 });
